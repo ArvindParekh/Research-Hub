9e634edd40b3549382a6c5c0ade398c8e72fed92
ArvindParekh <aruparekh@gmail.com>
2025-11-17 23:48:25 -0500

    fix(prisma-prod): install prisma nextjs monoreo workaround plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,30 @@
 /** @type {import('next').NextConfig} */
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb', // Increase body size limit for base64 images
-    },
-  },
-  // Add image domains if using external images
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
+   eslint: {
+      ignoreDuringBuilds: true,
+   },
+   webpack: (config, { isServer }) => {
+      if (isServer) {
+         config.plugins = [...config.plugins, new PrismaPlugin()];
+      }
+      return config;
+   },
+   experimental: {
+      serverActions: {
+         bodySizeLimit: "10mb", // Increase body size limit for base64 images
       },
-    ],
-  },
+   },
+   // Add image domains if using external images
+   images: {
+      remotePatterns: [
+         {
+            protocol: "https",
+            hostname: "**",
+         },
+      ],
+   },
 };
 
 export default nextConfig;
